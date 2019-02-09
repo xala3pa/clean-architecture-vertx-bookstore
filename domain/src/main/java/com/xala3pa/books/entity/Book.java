ae91c9924e9a55f3b5220d727ef2d246dfcac100
xala3pa <salazar3pa@gmail.com>
2019-02-09 11:55:27 +0100

    Domain Data added

---
+++ domain/src/main/java/com/xala3pa/books/entity/Book.java
@@ -0,0 +1,24 @@
+package com.xala3pa.books.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Book {
+
+  private Long isbn;
+  private String title;
+  private String description;
+  private BookCategory bookCategory;
+  private BookStatus bookStatus;
+  private String author;
+  private Date dateAdded;
+  private Date consumed;
+
+}
---
+++ domain/src/main/java/com/xala3pa/books/entity/BookCategory.java
@@ -0,0 +1,5 @@
+package com.xala3pa.books.entity;
+
+public enum BookCategory {
+  FICTION, TECHNICAL, FANTASY, HISTORY, COOK, NONFICTION, MYSTERY
+}

f7adef5c381e0317b13847ab2fb03b60df4e9247
xala3pa <salazar3pa@gmail.com>
2019-02-18 22:44:40 +0100

    Simple test using REST assured

---
+++ application/src/test/java/com/xala3pa/books/RestVertxApplicationRestTest.java
@@ -0,0 +1,37 @@
+package com.xala3pa.books;
+
+import io.restassured.RestAssured;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class RestVertxApplicationRestTest {
+
+  public static final int HTTP_STATUS_CODE_OK = 200;
+
+  @BeforeClass
+  public static void setUp() {
+    RestAssured.baseURI = "http://localhost";
+    RestAssured.port = Integer.getInteger("http.port", 8080);
+  }
+
+  @AfterClass
+  public static void tearDown() {
+    RestAssured.reset();
+  }
+
+  @Test
+  public void shouldReturnUserForGetWithCorrectId() {
+
+    given().
+            header("Accept-Encoding", "application/json")
+            .when()
+            .get("/books")
+            .then()
+            .assertThat()
+            .statusCode(HTTP_STATUS_CODE_OK);
+  }
+
+}
